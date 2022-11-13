d4ed3990fe6aaf96ec6684228209392412e92eb6
Ashraful Alam <ashrafcse3@gmail.com>
2022-11-13 15:03:35 +0000

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div
+            style={{
+                background: `url(${footer})`,
+                backgroundSize: 'contain',
+            }}
+        >
+            <footer className="footer p-10">
+                <div className='ml-9'>
+                    <span className="footer-title">Services</span>
+                    <Link className="link link-hover">Branding</Link>
+                    <Link className="link link-hover">Design</Link>
+                    <Link className="link link-hover">Marketing</Link>
+                    <Link className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link className="link link-hover">About us</Link>
+                    <Link className="link link-hover">Contact</Link>
+                    <Link className="link link-hover">Jobs</Link>
+                    <Link className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link className="link link-hover">Terms of use</Link>
+                    <Link className="link link-hover">Privacy policy</Link>
+                    <Link className="link link-hover">Cookie policy</Link>
+                </div>
+            </footer>
+            <p className="footer-center p-4">Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
         </div>
     );
 };
